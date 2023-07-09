834fc83a4d28f4b80f3a4ca9223e93f54157cefc
rogerio.martins <rogerio.martins@merkle.com>
2023-07-09 14:15:00 +0100

    fetch data

--- expenses-chart-component/script.js
+++ expenses-chart-component/script.js
@@ -1,1 +1,41 @@
-console.log("Hello");
+const ctx = document.getElementById("myChart");
+
+async function fetchData() {
+  const url = "./data.json";
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchData().then((data) => {
+  labels = [];
+  values = [];
+  for (i = 0; i < data.length; i++) {
+    labels.push(data[i].day);
+    values.push(data[i].amount);
+  }
+
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          data: values,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
