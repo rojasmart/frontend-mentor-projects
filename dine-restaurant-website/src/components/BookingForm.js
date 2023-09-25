e865cc7c24fae97c1863591158a5221b4f5775b3
binarypsilocybin <rogeriosvaldo@gmail.com>
2023-09-25 20:49:53 +0100

    select arrow styling

--- dine-restaurant-website/src/components/BookingForm.js
+++ dine-restaurant-website/src/components/BookingForm.js
@@ -14,45 +14,55 @@ const BookingForm = () => {
           <form>
             <div className="booking-contacts">
               <label htmlFor="">
-                <input type="text" />
+                <input type="text" placeholder="name" />
               </label>
               <label htmlFor="">
-                <input type="text" />
+                <input type="text" placeholder="email" />
               </label>
             </div>
             <div className="booking-schedule">
               <fieldset>
-                <legend>Pick a date</legend>
-                <label>
-                  <input type="number" placeholder="DD" />
-                </label>
-                <label>
-                  <input type="number" placeholder="MM" />
-                </label>
-                <label>
-                  <input type="number" placeholder="YYYY" />
-                </label>
+                <div className="label-booking">
+                  <legend>Pick a date</legend>
+                </div>
+                <div className="inputs-booking">
+                  <label>
+                    <input type="number" placeholder="DD" />
+                  </label>
+                  <label>
+                    <input type="number" placeholder="MM" />
+                  </label>
+                  <label>
+                    <input type="number" placeholder="YYYY" />
+                  </label>
+                </div>
               </fieldset>
               <fieldset>
-                <legend>Pick a time</legend>
-                <label>
-                  <input type="number" placeholder="09" />
-                </label>
-                <label>
-                  <input type="number" placeholder="00" />
-                </label>
-                <label>
-                  <select>
-                    <option value="am">AM</option>
-                    <option value="pm">PM</option>
-                  </select>
-                </label>
+                <div className="label-booking">
+                  <legend>Pick a time</legend>
+                </div>
+                <div className="inputs-booking">
+                  <label>
+                    <input type="number" placeholder="09" />
+                  </label>
+                  <label>
+                    <input type="number" placeholder="00" />
+                  </label>
+                  <label>
+                    <div className="select-booking">
+                      <select>
+                        <option value="am">AM</option>
+                        <option value="pm">PM</option>
+                      </select>
+                    </div>
+                  </label>
+                </div>
               </fieldset>
             </div>
           </form>
           <div className="counter-form">
             <button onClick={() => setCounter(count - 1)}>-</button>
-            <div className="counter-number">{count}</div>
+            <div className="counter-number">{count} people</div>
             <button onClick={() => setCounter(count + 1)}>+</button>
           </div>
           <button className="button-submit">make a reservation</button>
