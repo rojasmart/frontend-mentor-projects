0ee18fa974b662f330dcac60f17a5773b4dfe124
binarypsilocybin <rogeriosvaldo@gmail.com>
2023-10-13 16:20:37 +0100

    new project and layout design

---
+++ interactive-pricing-component/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main>
+      <header>
+        <h1>Simple, traffic-based pricing</h1>
+        <p>Sign-up for our 30-day trial. No credit card required. </p>
+      </header>
+      <div className="card"></div>
+    </main>
+  );
+}
+
+export default App;
