edf66f3be6f520d190d0b1876c9a6ff9ff46b7cd
binarypsilocybin <rogeriosvaldo@gmail.com>
2023-10-24 17:25:52 +0100

    added style for the component

--- interactive-comments-section/script.js
+++ interactive-comments-section/script.js
@@ -1,1 +1,40 @@
-console.log("hello");
+const commentsName = document.querySelector(
+  ".comments-current-user .comments-name"
+);
+
+const commentsLike = document.querySelector(
+  ".comments-current-user .comments-like-number"
+);
+
+const commentsImage = document.querySelector(
+  ".comments-current-user .comments-avatar img"
+);
+
+const commentsDate = document.querySelector(
+  ".comments-current-user .comments-date "
+);
+
+const commentsContent = document.querySelector(
+  ".comments-current-user .comments-content "
+);
+
+const fetchData = async () => {
+  try {
+    const res = await fetch("./data.json");
+    const data = await res.json();
+    buildData(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchData();
+
+const buildData = (data) => {
+  data.comments.forEach((comment) => {
+    console.log(comment);
+    commentsName.textContent = data.currentUser.username;
+    commentsImage.src = data.currentUser.image.png;
+    commentsContent.textContent = data.content;
+  });
+};
